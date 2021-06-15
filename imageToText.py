a98dd6e58dbeda4ba586e27f623a7f8e65e426a5
Vineet <vineet.sgr07@gmail.com>
2021-06-15 21:37:10 +0530

    Added format

--- imageToText.py
+++ imageToText.py
@@ -7,17 +7,22 @@ from pytesseract import image_to_string, image_to_boxes
 def image_to_Text(path):
     img = cv.imread(path)
     actualImage = img.copy()
+
+
     imgGray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     imgCanny = cv.Canny(imgGray, 150, 200)
     imageBox = img.shape
     imgBlank = np.zeros_like(imgGray)
     kernel = np.ones((5, 5), np.uint8)
     imgDialation = cv.dilate(imgCanny, kernel, iterations=10)
+
     trimLeftSectionX, trimRightSectionY, croppedImage, croppedDialationImage = getContours(imgDialation, actualImage)
     tripImageDimension = trimImage(imgDialation)
+
     imgStack = stackImages(0.6, ([imgDialation, croppedDialationImage, croppedImage], [imgGray, actualImage, croppedImage]))
     im_pil = Image.fromarray(croppedImage)
     im_np = np.asarray(im_pil)
+
     characterWithText = image_to_boxes(im_pil)
     imageWithText = image_to_string(im_pil)
     # cv.imshow("stack", imgStack)
